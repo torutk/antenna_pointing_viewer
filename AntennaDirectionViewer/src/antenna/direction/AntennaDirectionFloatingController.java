1cc72567ecb5b4790146d1da0744eef16248ee51
TAKAHASHI,Toru <torutk@gmail.com>
2019-06-15 08:25:29 +0900

    Fix label width in floating window, Add copy to clipboard in floating window.

--- AntennaDirectionViewer/src/antenna/direction/AntennaDirectionFloatingController.java
+++ AntennaDirectionViewer/src/antenna/direction/AntennaDirectionFloatingController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -28,37 +31,64 @@ public class AntennaDirectionFloatingController implements Initializable {
     private double dragStartX;
     private double dragStartY;
     private Scene scene;
+    private Clipboard clipboard;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        azimuthLabel.textProperty().bind(model.azimuthProperty().asString());
-        elevationLabel.textProperty().bind(model.elevationProperty().asString());
-        polarizationLabel.textProperty().bind(model.polarizationProperty().asString());
+        azimuthLabel.textProperty().bind(model.azimuthProperty().asString("%+7.2f"));
+        elevationLabel.textProperty().bind(model.elevationProperty().asString("%5.2f"));
+        polarizationLabel.textProperty().bind(model.polarizationProperty().asString("%+6.2f"));
+        antennaTargetLabel.textProperty().bind(Bindings.concat("ANT#", model.targetAntennaProperty()));
+        angleModeLabel.textProperty().bind(Bindings.createStringBinding(() ->
+                model.angleModeProperty().isNull().get() ? "-" : model.angleModeProperty().get().name().substring(0, 1),
+                model.angleModeProperty())
+        );
+
+        // set handlers to a scene when it is available.
         azimuthLabel.sceneProperty().addListener((observable, oldScene, newScene) -> {
             if (newScene != null) {
                 scene = newScene;
-                scene.setOnMousePressed(e -> {
-                    dragStartX = e.getSceneX();
-                    dragStartY = e.getSceneY();
-                });
-                scene.setOnMouseDragged(e -> {
-                   getStage().setX(e.getScreenX() - dragStartX);
-                   getStage().setY(e.getScreenY() - dragStartY);
-                });
-                scene.setOnMouseClicked(e -> {
-                    if (e.getClickCount() == 2) {
-                        model.setFloating(false);
-                    }
-                });
+                initializeSceneHandler();
             }
         });
-        antennaTargetLabel.textProperty().bind(Bindings.concat("ANT#", model.targetAntennaProperty()));
-        angleModeLabel.textProperty().bind(Bindings.createStringBinding(() ->
-                model.angleModeProperty().isNull().get() ? "-" : model.angleModeProperty().get().name().substring(0, 1))
+    }
+
+    // initialize handlers of scene
+    private void initializeSceneHandler() {
+        scene.setOnMousePressed(e -> {
+            dragStartX = e.getSceneX();
+            dragStartY = e.getSceneY();
+        });
+        scene.setOnMouseDragged(e -> {
+            getStage().setX(e.getScreenX() - dragStartX);
+            getStage().setY(e.getScreenY() - dragStartY);
+        });
+        scene.setOnMouseClicked(e -> {
+            if (e.getButton() == MouseButton.PRIMARY && e.getClickCount() == 2) {
+                model.setFloating(false);
+            } else if (e.getButton() == MouseButton.SECONDARY) {
+                copyClipboard();
+            }
+        });
+    }
+
+    // copy data to system clipboard as text.
+    private void copyClipboard() {
+        clipboard = Clipboard.getSystemClipboard();
+        var content = new ClipboardContent();
+        content.putString(toText());
+        clipboard.setContent(content);
+    }
+
+    // create text from view data
+    private String toText() {
+        return String.format("%s Az %s El %s Pol %s %s",
+                antennaTargetLabel.getText(), azimuthLabel.getText(), elevationLabel.getText(),
+                polarizationLabel.getText(), angleModeLabel.getText()
         );
     }
 
-    Stage getStage() {
+    private Stage getStage() {
         return (Stage) scene.getWindow();
     }
 }
